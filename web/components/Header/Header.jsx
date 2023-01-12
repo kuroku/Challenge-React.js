7b7859adcd6065eb260eda978c82c26040e9e0e6
Robert Montilla <robertmontilla94@gmail.com>
2023-01-12 17:28:18 -0400

    Checks if the user can see the Tabs of the Header component

--- web/components/Header/Header.jsx
+++ web/components/Header/Header.jsx
@@ -1,15 +1,18 @@
 import style from './header.module.css'
 import srcLogo from '../../assets/logo.png'
+import useAuth from '../../hooks/useAuth'
 
 export default function Header() {
+  const auth = useAuth()
   return (
     <header id={style.appbar}>
       <img src={srcLogo} alt="Condor's" />
-      <ul>
+      { auth.session && ( <ul>
         <li>Search</li>
         <li>Profile</li>
         <li>Logout</li>
-      </ul>
+      </ul>)}
+     
     </header>
   )
 }
